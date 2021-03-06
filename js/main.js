bbe1d2c05080402be572f80f657de0a5bd64741d
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-03-06 03:51:15 -0600

    deploy test

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 /*get the slider wrapper width*/
-let wrapperWidth =  document.querySelector(".slider-wrapper").clientWidth;
+let wrapperWidth = document.querySelector(".slider-wrapper").clientWidth;
 
-console.log( "wrapper", wrapperWidth )
+console.log("wrapper", wrapperWidth)
 
 /*slider quantity*/
 let slides = 8
 /*get the slider tray width*/
 document.querySelector(".slider-tray").style.width = `${wrapperWidth * slides}px`
 
-console.log("tray", document.querySelector(".slider-tray").offsetWidth )
+console.log("tray", document.querySelector(".slider-tray").offsetWidth)
 
 console.log(wrapperWidth)
 
@@ -26,32 +26,84 @@ let slideIndex = 1;
 document.querySelector(".slider-tray")
     .style.transform = `translate3d(${-captionWidth}px, 0, 0)`
 
-console.log( document.querySelector(".slider-caption").offsetWidth)
+console.log(document.querySelector(".slider-caption").offsetWidth)
 
 //
-document.getElementById("forward").addEventListener("click", () => {
+
+const forward = () => {
     document.querySelector(".slider-tray")
+        .style.transition = `transform 1s ease`
+
+    document.querySelector(".slider-tray").style.transform = `translate3d(${(displacement * -1)}px, 0, 0)`
+
+    console.log( "displacement before", displacement )
+
+    displacement += captionWidth
+    console.log( "displacement after", displacement )
+    slideIndex++
+    console.log(displacement)
+    console.log(slideIndex)
+    if (slideIndex === (slides - 1)) {
+        setTimeout(() => {
+            document.querySelector(".slider-tray")
+                .style.transition = `unset`
+            document.querySelector(".slider-tray")
+                .style.transform = `translate3d(${-captionWidth}px, 0, 0)`
+        }, 1000)
+
+        displacement = captionWidth * 2
+        slideIndex = 1
+    }
+}
+
+
+const backward = () => {
+    
+
+    if ( slideIndex === 1 ){
+        document.querySelector(".slider-tray")
+        .style.transition = `transform 1s ease`
+            document.querySelector(".slider-tray").style.transform = `translate3d(0px, 0, 0)`
+        setTimeout(() => {
+            document.querySelector(".slider-tray")
+                .style.transition = `unset`
+            document.querySelector(".slider-tray")
+                .style.transform = `translate3d(${-(captionWidth * (slides - 2))}px, 0, 0)`
+        }, 1000)
+        displacement = captionWidth * 6
+        slideIndex = 6
+    } else {
+        console.log( slideIndex )
+        slideIndex--
+        displacement -= captionWidth
+        document.querySelector(".slider-tray")
             .style.transition = `transform 1s ease`
+    
+        document.querySelector(".slider-tray").style.transform = `translate3d(${(displacement * -1)}px, 0, 0)`
+    
+        
+        
+    }
+    /*document.querySelector(".slider-tray")
+        .style.transition = `transform 1s ease`
     document.querySelector(".slider-tray").style.transform = `translate3d(${(displacement * -1)}px, 0, 0)`
 
     displacement += captionWidth
     slideIndex++
-    console.log( displacement)
-    console.log( slideIndex)
-    if( slideIndex === ( slides - 1 ) ){
-        setTimeout( () => {
+    console.log(displacement)
+    console.log(slideIndex)
+    if (slideIndex === (slides - 1)) {
+        setTimeout(() => {
             document.querySelector(".slider-tray")
-            .style.transition = `unset`
+                .style.transition = `unset`
             document.querySelector(".slider-tray")
-            .style.transform = `translate3d(${-captionWidth}px, 0, 0)`
-        },1000)
-        
+                .style.transform = `translate3d(${-captionWidth}px, 0, 0)`
+        }, 1000)
+
         displacement = captionWidth * 2
         slideIndex = 1
-    }
-    /*if( slideIndex === 7){
-        
-        displacement = 598.41
-        slideIndex = 0
     }*/
-})
+}
+
+document.getElementById("forward").addEventListener("click",  forward)
+document.getElementById("backward").addEventListener("click",  backward)
