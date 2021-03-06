5fdb0766e6af37c3c1a4e6eaea172d612afa460c
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2021-03-06 04:52:28 -0600

    forward & backward complete

--- js/main.js
+++ js/main.js
@@ -1,22 +1,17 @@
+/*slider quantity*/
+let slides = 8
+
 /*get the slider wrapper width*/
 let wrapperWidth = document.querySelector(".slider-wrapper").clientWidth;
 
-console.log("wrapper", wrapperWidth)
-
-/*slider quantity*/
-let slides = 8
-/*get the slider tray width*/
+/*set the slider tray width*/
 document.querySelector(".slider-tray").style.width = `${wrapperWidth * slides}px`
-
-console.log("tray", document.querySelector(".slider-tray").offsetWidth)
-
-console.log(wrapperWidth)
 
 /*get the caption width*/
 let captionWidth = document.querySelector(".slider-caption").offsetWidth
 
 /*setting the displacement ( equals to caption width)*/
-let displacement = captionWidth * 2;
+//let displacement = -captionWidth;
 
 /*starting slide*/
 let slideIndex = 1;
@@ -24,12 +19,46 @@ let slideIndex = 1;
 /*setting start position*/
 
 document.querySelector(".slider-tray")
-    .style.transform = `translate3d(${-captionWidth}px, 0, 0)`
+    .style.transform = `translate3d(-${slideIndex * captionWidth}px, 0, 0)`
 
-console.log(document.querySelector(".slider-caption").offsetWidth)
+const forward = () => {
+    document.querySelector(".slider-tray")
+        .style.transition = `transform 1s ease`
+        document.querySelector(".slider-tray")
+            .style.transform = `translate3d(-${(slideIndex + 1) * captionWidth}px, 0, 0)`
+    slideIndex++
+    if( slideIndex === slides - 1 ){
+        console.log( "este es el penúltimo slide" )
+        setTimeout(() => {
+            document.querySelector(".slider-tray")
+                .style.transition = `unset`
+            slideIndex = 1
+            document.querySelector(".slider-tray")
+                .style.transform = `translate3d(-${slideIndex * captionWidth}px, 0, 0)`
+        }, 1000)
+    }
+}
 
-//
+const backward = () => {
+    document.querySelector(".slider-tray")
+        .style.transition = `transform 1s ease`
+        document.querySelector(".slider-tray")
+            .style.transform = `translate3d(-${(slideIndex - 1) * captionWidth}px, 0, 0)`
+    slideIndex--
+    if( slideIndex == 0){
+        console.log(" este es el primer slide ")
+        setTimeout(() => {
+            document.querySelector(".slider-tray")
+                .style.transition = `unset`
+            slideIndex = slides - 2
+            document.querySelector(".slider-tray")
+                .style.transform = `translate3d(-${slideIndex * captionWidth}px, 0, 0)`
+        }, 1000)
+    }
+}
 
+
+/*
 const forward = () => {
     document.querySelector(".slider-tray")
         .style.transition = `transform 1s ease`
@@ -84,26 +113,9 @@ const backward = () => {
         
         
     }
-    /*document.querySelector(".slider-tray")
-        .style.transition = `transform 1s ease`
-    document.querySelector(".slider-tray").style.transform = `translate3d(${(displacement * -1)}px, 0, 0)`
-
-    displacement += captionWidth
-    slideIndex++
-    console.log(displacement)
-    console.log(slideIndex)
-    if (slideIndex === (slides - 1)) {
-        setTimeout(() => {
-            document.querySelector(".slider-tray")
-                .style.transition = `unset`
-            document.querySelector(".slider-tray")
-                .style.transform = `translate3d(${-captionWidth}px, 0, 0)`
-        }, 1000)
-
-        displacement = captionWidth * 2
-        slideIndex = 1
-    }*/
 }
+
+*/
 
 document.getElementById("forward").addEventListener("click",  forward)
 document.getElementById("backward").addEventListener("click",  backward)
